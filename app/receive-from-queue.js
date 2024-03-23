49cd0d897015a6d18eda613b8f8140b7743b9052
Rana Salem <ranatasalem@gmail.com>
2024-03-23 09:13:12 +0000

    format console log to return first and last name (not tested)

--- app/receive-from-queue.js
+++ app/receive-from-queue.js
@@ -1,32 +1,33 @@
-const { delay, ServiceBusClient } = require('@azure/service-bus')
+const { delay, ServiceBusClient } = require("@azure/service-bus");
 
-const connectionString = process.env.ASB_CONN_STR
-const queue = process.env.ASB_QUEUE
-const sbClient = new ServiceBusClient(connectionString)
-const receiver = sbClient.createReceiver(queue)
+const connectionString = process.env.ASB_CONN_STR;
+const queue = process.env.ASB_QUEUE;
+const sbClient = new ServiceBusClient(connectionString);
+const receiver = sbClient.createReceiver(queue);
 
 const receiveFromQueue = async () => {
   try {
     const handleMessage = async (message) => {
-      console.log(`${message.body}`)
-    }
+      const { firstName, lastName } = JSON.parse(message.body);
+      console.log(`${firstName} ${lastName}`);
+    };
 
     const handleError = async (error) => {
-      console.error(`Error: ${error}`)
-    }
+      console.error(`Error: ${error}`);
+    };
 
     receiver.subscribe({
       processMessage: handleMessage,
-      processError: handleError
-    })
+      processError: handleError,
+    });
 
-    await delay(10000)
+    await delay(10000);
   } catch (error) {
-    console.error(`Error: ${error}`)
+    console.error(`Error: ${error}`);
   } finally {
-    await receiver.close()
-    await sbClient.close()
+    await receiver.close();
+    await sbClient.close();
   }
-}
+};
 
-module.exports = receiveFromQueue
+module.exports = receiveFromQueue;
